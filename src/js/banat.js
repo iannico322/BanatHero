44553c69665b5bb46858ef41f2c593d7b4d6b9c9
Zer0 <73187566+iannico322@users.noreply.github.com>
2026-02-18 10:21:38 +0800

    Update banat.js

--- src/js/banat.js
+++ src/js/banat.js
@@ -1,5 +1,104 @@
 let haveIt = [];
-let banats = null;
+let banats = {
+    "banats":{
+        "normal":[
+            "Kung akoa ka, yahay kayka",
+            "kung ang english ng asol ay blue, bakit palaging ikaw naiisip ko",
+            "Ang gugma murag ako, so gugmaa nako",
+            "Kung magkakaroon ako ng sariling planeta, gusto ko ikaw lang ang axis nito, ....para sayo lang iikot ang mundo ko.",
+            "Para tayong kwento ni Juan Tamad. Ako si Juan Tamad at ikaw yung bayabas...Hinihintay lang kitang mahulog sa akin.",
+            " Sana exam mo nalang din ako... para sagutin mo din ako.",
+            "Ang galing mo din ano? Di mo pa nga ako binabato, tinamaan na ko sayo.",
+            "Sana ako lang si antok...para gabi gabi pwede kitang dalawin",
+            "Dalawa lang ang pwedeng mangyari. Either magmurahan tayo or magmahalan tayo",
+            "Tindera ka ba ng yosi? kasi you give me HOPE..and MORE",
+            "Magaling ka ba sa algebra? Can you substitute my 'x'?",
+            "Calculator ka ba? Kasi, sa 'yo pa lang, solved na ko.",
+            "Peter pan said when you think of happy things you fly but when I thought about you I fell.",
+            "kung si Ed Caluag may nakikitang iba, ako wala ikaw lang talaga.",
+            "ikakagalit ba ni Jessica sojo kung d umano akoy nahulog sayo",
+            "I don't like math but I lab yo so maths",
+            "My favorite word is universe cause it start with u n i",
+            "My parents said to focus on my future. so im focusing on you",
+            " I love \"W\" but \"double\" is silent.",
+            "do you love yourself? i think we love the same person",
+            "fliptop ka ng fliptop wla ka namang lolipop dto ka nlang sakin mamahalin kita nonestop yieeeee",
+            "wow you're mom must be a great baker, cuz she made such a cutie pie",
+            "I have humor but i love hu-mor",
+            "I thought happiness is starting with \"H\" but it's actually starting in \"U\".",
+            "No pen no paper but you still draw my attention",
+            "What if you are given a chance to talk to your future? Ano pag uusapan natin?",
+            "are u a secret? cuz i don't think i can share u",
+            "Para na akong london bridge dahil sayo. I'm falling down",
+            "Kung si rizal tinamaan sa likod,ako naman tinamaan sayo",
+            "baliktarin mo ung tapat oh diba walang nagbago ikaw parin mahal ko",
+            "Gwapo ko"
+            ],
+        "techy":[
+            "A life without you, would be like a computer without an OS.",
+            "Are you a double? The thought of you always floats inside my head.",
+            "Are you an applet? You make me feel all GUI (gooey) inside.",
+            "Baby are you a motherboard?, Cause I'd \"RAM\" you all night long.",
+            "Baby you must be Google Glasses, because you augment my reality",
+            "Baby, if they made you in C, you would have a pointer to my heart.",
+            "Baby, you make my floppy disk turn into a hard drive",
+            "Baby, you must be running a TCP protocol, since every time I talk to you, your body gives me an acknowledgment!",
+            "Are you a software update? Because you make my heart race with every new version.","Do you believe in love at first byte, or should I refresh your cache?","If you were a code, you'd be an elegant algorithm, because you've optimized my life.","Are you a firewall? Because you've blocked all other thoughts from my mind.","You must be a developer, because you've just coded your way into my heart.","You must be a server, because when I look at you, I feel like I'm in the cloud.","If you were a function, you'd be the one I'd always call.","Is your heart a hard drive? Because it seems like I've found the perfect storage for my love.","You must be a web designer because you've just created a beautiful page in my life.","Are you a bit? Because you're the smallest, yet most significant part of my world.","Is your heart encrypted? Because I can't seem to decode your feelings for me.","Are you a coding language? Because you make my heart compile with joy.","Are you an API? Because you've opened up a world of possibilities for us.","Is your name Bluetooth? Because I'm really feeling a wireless connection between us.","Is your love password-protected? Because I'm hoping to be the one who knows the key.","You must be a database query because you've retrieved my affection.","Are you a tech support agent? Because you've just solved the bug of my heart.",
+            "Before you, I was a PC without a power outlet.","You must be a pixel, because your love has given color to my world.","Is your heart a code repository? Because I want to commit to you forever.","You must be a computer virus because you've infected me with love.","Is your name JavaScript? Because you make everything on my web page function better.","You must be a software update because being with you feels like a constant improvement",
+            "Can I stick my USB drive in your USB port?",
+            "Can you be my ActionListener? That way you notice everything that I do.",
+            "Can you be my private variable? I want to be the only one with access to you.",
+            "Do u like me? Text '1' for 'Yes,' '2' for 'No.",
+            "hey, are you Wi-Fi? Cuz im feeling the connection!",
+            "You are hotter than the bottom of my laptop.",
+            "01001001 00100000 01101100 01101111 01110110 01100101 00100000 01111001 01101111 01110101 00100000 01101101 01101111 01110010 01100001 00100000 01110100 01101000 01100001 01101110 00100000 00110011 00110000 00110000 00110000",
+            "Hey baby, lets turn off our firewalls and connect our Ethernet cable.",
+            "I am a Boolean method whose love will always return true.",
+            "I am the field attribute in your class: I can't exist unless you do.",
+            "I googled your name earlier... I clicked on 'I'm Feeling Lucky.'",
+            "I think we should increase our bandwidth.",
+            "I think you could be an integral part of my project life cycle.",
+            "I think you're my compiler. My life wouldn't start without you.",
+            "I would love to stick my pins into your sockets.",
+            "I'll always have cache for you.",
+            "If I was an operating system, your process would have top priority.",
+            "If I were a method, you must be my parameter, because I will always need you.",
+            "Is your name Google? Because you have everything I've been searching for.",
+            "Isn't your e-mail\n\n address\n beautifulgirl@mydreams.com?",
+            "Let me be the 'throws Exception' to your 'public static void main (String[] args)'. I will accept whatever you give me.",
+            "My love for you is a constant variable: unupdatable and unchangeable.",
+            "My love is a for loop without the increment operator— infinitive, non-terminating, and difficult to stop once it starts running",
+            "My main method is 'public love iLoveYou().'",
+            "Nobody turns me on from a cold boot like you.",
+            "Trust me, I'm user friendly.",
+            "We are an aggregation of classes: one cannot exist without the other.",
+            "You are my increment operator. You make my value increase.",
+            "You are my semicolon; always present in everything I do.",
+            "You are the IDE of my life: I find it easier because of you.",
+            "You auto-complete me."
+          ],
+        "bisaya":["","Kinsay may di mapian anang kada adlaw mahulog nimo","kung ang coconut kay lubi, kiss ko bi","sa ngalan ni Jopay wag ka ng mawala","Muupos man ang kandila mungitngit man ang kagabhiun, ikaw ra japon ang akong huna-hunaon","Akong gugma kanimo mura kini og kadagatan, makita nimo ang sinugdanan pero dili ang katapusan","oh inday, hakuton kong tanang bahandi matagamtaman ko lang ang matam-is mong giti","Dahun malaya, puthaw mataya, pero ang pag-higugma ko nimo dili Yaga-yaga","ikaw ang itik nga ga kapa kapa sa akong malapokong dughan","Kung imong gi dala ang sumpa ni medusa mag tan aw gipun kos imong mata para akong bato nga lawas mag atobang gihapun sa imohang ka gwapa","Mawala man ang kahayag sa bulan ug mga bituon, mo bugnaw man pag ayo ang panahon, di naman ko mo mata sa kabuntagon Ikaw gihapon akong pilion","Andam tikang hulaton mabugnaw man ang kan on, malaya man ang dahon, basta kitang duha ang magkadayon","Ikaw ray naka pasiga sa na ponder kung kasing²","Sauna daghan kaayog animal sa akong kinabuhi pero karun cow nalang","Ana sila kung naay mohawa naay moabot ah bahalag walay moabot basta di lang ka mo hawa",
+    "Sa tanan tawo dinhi sa kalibutan, ikaw ang akong napili nga kauban.", "Bawion ta ang adlaw kada gabii, kay imo ang bituon sa akong kahayag.", "Sa imong mga mata, nakita nako ang kahayag sa akong kasingkasing.", "Ikaw akong panaginip nga nauyon ko nga di pa matuman.", "Sa imong gugma, nahibal-an ko ang kahulogan sa kahayag.", "Ang tanang adlaw na masayran ka mao ang hinungdan sa akong pagtambay diri kay nahigugma ko kanimo.", "Di ko malupigan ang imong gugma, bisag unsa pa ka komplikado.", "Ikaw siguro gikan sa langit, kay naay angel nga nautot.", "Ambot lang kung asa ka gikan, basta gwapa ka sa akong panan-aw.",
+
+
+
+
+
+
+    "Sa tanan tawo dinhi sa kalibutan, ikaw ang akong napili nga gusto ma kauban.", "Bawion ta ang adlaw kada gabii, kay ihatag ko nimo ang gugmang maanyag.", "Sa imong mga mata, nakita nako ang kahayag sa akong kasingkasing.", "Ikaw akong damgo nga nauyon ko nga gusto ko makab-ot.", "Tungod sa imo nasabtan ko kung unsa gayod and gugma", "Ang masayran ka kada adlaw mao ang hinungdan sa akong pagtambay diri sa kalibutan.", "Di ko malupigan ang imong gugma, bisag unsa pa ka komplikado.", "Ikaw siguro gikan sa langit, kay naay inig makita tika mo siga akong palibot", "Unsay buot pasabot sa imong nawong, para ma bal-an ko kung bagay ba jud ta", "Ambot lang kung asa ka gikan, basta gwapa/gwapo ka sa akong panan-aw.",
+    "Ikaw akong pasko ug bag-ong tuig matag adlaw.",
+    "Kanimo ko gibutang akong kasingkasing kay kini imong lalum nga bahangdi.", "Nalipay kaayo ko nga nahibal-an nimo kung unsa ka importante kanako.", "Ang akong kasingkasing usa ka libro, ug ikaw ang pinakamahinangop nga pahina.", "Sa tanan mga butang sa kalibutan, imong gugma ang pinakamaayo.", "Bisag unsaon pa, dili gyud ko makalimot kanimo.","Sa atong duha, imong beauty kay high-speed internet.","Sa imong mga halok, matubag ang tanan akong pangutana.",
+    "Kanang imong ka sweet, murag ice cream sa init nga adlaw.","Sa mga tinuod nga storya, ikaw ra ang love interest sa akong nobela.","Sa kanta sa gugma, ikaw ra ang constant sa akong playlist.","Kung imo lang mahibaw-an unsay gibati sa akong kasingkasing, mag-TikTok challenge na ko.","Sa atong mga away, ikaw ra gihapon akong favorite opponent.","Sa tanan'g mga emojis, ikaw ang heart eyes sa akong keyboard."
+,"Kung i-love triangle ang atong storya, ikaw gihapon ang center.","Sa akong tanan'g search history, ikaw ra ang result.","Kanang imong smile, murag sunshine sa akong adlaw.","Sa atong duha, imo ang missing piece sa akong puzzle.","Kanang imong mga mata ang misteryo nga akong gipangita","Kung tag balay imong gugma, di ko gyud mo lakaw pa.","Sa pasko, ikaw ang akong 'parol,' nga nagdala og kahayag sa akong kinabuhi.","Kanang mga kahayag sa pasko, dili gyud maigo sa imong mata.","Sa karong pasko, imong gugma mao ang akong pinakagwapa'ng Christmas tree."
+
+
+
+
+    
+    ]
+        
+    }
+};
 let banat_choice = [];
 
 // Load JSON data first, then initialize the app
